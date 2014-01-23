f58d9339ded95377cc5f802401833ea0e1bfb45c
runigma <nigmatullin.ruslan@gmail.com>
2014-01-24 01:32:50 +0400

    New class GroupData added
    New class GroupData added to project.

--- adressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ adressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -30,7 +30,7 @@ public class GroupCreationTests {
     openMainPage();
     gotoGroupsPage();
     initGroupCreation();
-    fillGroupForm("group 1", "header 1", "footer 1");
+    fillGroupForm(new GroupData("group 1", "header 1", "footer 1"));
     submitGroupCreation();
     returnToGroupsPage();
   }
@@ -40,7 +40,7 @@ public class GroupCreationTests {
     openMainPage();
     gotoGroupsPage();
     initGroupCreation();
-    fillGroupForm("", "", "");
+    fillGroupForm(new GroupData("", "", ""));
     submitGroupCreation();
     returnToGroupsPage();
   }
@@ -53,13 +53,13 @@ private void submitGroupCreation() {
 	driver.findElement(By.name("submit")).click();
 }
 
-private void fillGroupForm(String name, String header, String footer) {
+private void fillGroupForm(GroupData group) {
 	driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys(name);
+    driver.findElement(By.name("group_name")).sendKeys(group.groupname);
     driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys(header);
+    driver.findElement(By.name("group_header")).sendKeys(group.header);
     driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys(footer);
+    driver.findElement(By.name("group_footer")).sendKeys(group.footer);
 }
 
 private void initGroupCreation() {
