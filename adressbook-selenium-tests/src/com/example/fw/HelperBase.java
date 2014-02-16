05b233a67e685827f1dd60cb0742bb91ff4471c9
runigma <nigmatullin.ruslan@gmail.com>
2014-02-17 01:56:41 +0400

    selectByText method was modified
    selectByText method was modified. Now it handles null values in input
    variable.

--- adressbook-selenium-tests/src/com/example/fw/HelperBase.java
+++ adressbook-selenium-tests/src/com/example/fw/HelperBase.java
@@ -63,7 +63,10 @@ public abstract class HelperBase {
 	}	
 	
 	protected void selectByText(By locator, String text) {
-		new Select(driver.findElement(locator)).selectByVisibleText(text);
+		if (text != null) {
+			new Select(driver.findElement(locator)).selectByVisibleText(text);
+		}
+		
 	}
 	
 }
