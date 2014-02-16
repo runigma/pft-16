5280c92d644084a84df7439926e06538996964ab
runigma <nigmatullin.ruslan@gmail.com>
2014-02-16 22:03:22 +0400

    Init and Stop methods was added to ApplicationManager
    Init and Stop methods was added to ApplicationManager. SetUp and
    TearDown methods in TestBase class modified by using Init and Stop
    methods.

--- adressbook-selenium-tests/src/com/example/fw/ApplicationManager.java
+++ adressbook-selenium-tests/src/com/example/fw/ApplicationManager.java
@@ -1,9 +1,15 @@
 package com.example.fw;
 
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import com.example.tests.ContactData;
@@ -11,85 +17,107 @@ import com.example.tests.GroupData;
 import com.example.tests.TestBase;
 
 public class ApplicationManager {
+
+	public static  WebDriver driver;
+	public static String baseUrl;
+	public static boolean acceptNextAlert = true;
+	private static StringBuffer verificationErrors = new StringBuffer();
+	
+	
+	public ApplicationManager(){
+	  driver = new FirefoxDriver();
+	  baseUrl = "http://localhost/";
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+	}
+	
+	public void stop() {
+	    driver.quit();
+	    String verificationErrorString = verificationErrors.toString();
+	    if (!"".equals(verificationErrorString)) {
+	      fail(verificationErrorString);
+	    }
+	}
+	
 	
 	// Methods for group creation	
 
 	public void returnToGroupsPage() {
-		TestBase.driver.findElement(By.linkText("group page")).click();
+		driver.findElement(By.linkText("group page")).click();
 	}
 
 	public void submitGroupCreation() {
-		TestBase.driver.findElement(By.name("submit")).click();
+		driver.findElement(By.name("submit")).click();
 	}
 
 	public void fillGroupForm(TestBase testBase, GroupData group) {
-		TestBase.driver.findElement(By.name("group_name")).clear();
-	    TestBase.driver.findElement(By.name("group_name")).sendKeys(group.name);
-	    TestBase.driver.findElement(By.name("group_header")).clear();
-	    TestBase.driver.findElement(By.name("group_header")).sendKeys(group.header);
-	    TestBase.driver.findElement(By.name("group_footer")).clear();
-	    TestBase.driver.findElement(By.name("group_footer")).sendKeys(group.footer);
+		driver.findElement(By.name("group_name")).clear();
+	    driver.findElement(By.name("group_name")).sendKeys(group.name);
+	    driver.findElement(By.name("group_header")).clear();
+	    driver.findElement(By.name("group_header")).sendKeys(group.header);
+	    driver.findElement(By.name("group_footer")).clear();
+	    driver.findElement(By.name("group_footer")).sendKeys(group.footer);
 	}
 
 	public void initGroupCreation() {
-		TestBase.driver.findElement(By.name("new")).click();
+		driver.findElement(By.name("new")).click();
 	}
 
 	public void gotoGroupsPage() {
-		TestBase.driver.findElement(By.linkText("groups")).click();
+		driver.findElement(By.linkText("groups")).click();
 	}
 
 	public void openMainPage() {
-		TestBase.driver.get(TestBase.baseUrl + "/addressbookv4.1.4/");
+		driver.get(baseUrl + "/addressbookv4.1.4/");
 	}
 
 	public void returnToHomePage() {
-		TestBase.driver.findElement(By.linkText("home page")).click();
+		driver.findElement(By.linkText("home page")).click();
 	}
 
 	// Methods for contact creation	
 	
 	public void submitContactCreation() {
-		TestBase.driver.findElement(By.name("submit")).click();
+		driver.findElement(By.name("submit")).click();
 	}
 
 	public void fillContactForm(TestBase testBase, ContactData contact) {
-		TestBase.driver.findElement(By.name("firstname")).clear();
-	    TestBase.driver.findElement(By.name("firstname")).sendKeys(contact.firstname);
-	    TestBase.driver.findElement(By.name("lastname")).clear();
-	    TestBase.driver.findElement(By.name("lastname")).sendKeys(contact.lastname);
-	    TestBase.driver.findElement(By.name("address")).clear();
-	    TestBase.driver.findElement(By.name("address")).sendKeys(contact.address);
-	    TestBase.driver.findElement(By.name("home")).clear();
-	    TestBase.driver.findElement(By.name("home")).sendKeys(contact.homephone);
-	    TestBase.driver.findElement(By.name("mobile")).clear();
-	    TestBase.driver.findElement(By.name("mobile")).sendKeys(contact.mobilephone);
-	    TestBase.driver.findElement(By.name("work")).clear();
-	    TestBase.driver.findElement(By.name("work")).sendKeys(contact.workphone);
-	    TestBase.driver.findElement(By.name("email")).clear();
-	    TestBase.driver.findElement(By.name("email")).sendKeys(contact.firstemail);
-	    TestBase.driver.findElement(By.name("email2")).clear();
-	    TestBase.driver.findElement(By.name("email2")).sendKeys(contact.secondemail);
-	    new Select(TestBase.driver.findElement(By.name("bday"))).selectByVisibleText(contact.day);
-	    new Select(TestBase.driver.findElement(By.name("bmonth"))).selectByVisibleText(contact.month);
-	    TestBase.driver.findElement(By.name("byear")).clear();
-	    TestBase.driver.findElement(By.name("byear")).sendKeys(contact.year);
-	    new Select(TestBase.driver.findElement(By.name("new_group"))).selectByVisibleText(contact.groupname);
-	    TestBase.driver.findElement(By.name("address2")).clear();
-	    TestBase.driver.findElement(By.name("address2")).sendKeys(contact.secondaddress);
-	    TestBase.driver.findElement(By.name("phone2")).clear();
-	    TestBase.driver.findElement(By.name("phone2")).sendKeys(contact.secondphone);
+		driver.findElement(By.name("firstname")).clear();
+	    driver.findElement(By.name("firstname")).sendKeys(contact.firstname);
+	    driver.findElement(By.name("lastname")).clear();
+	    driver.findElement(By.name("lastname")).sendKeys(contact.lastname);
+	    driver.findElement(By.name("address")).clear();
+	    driver.findElement(By.name("address")).sendKeys(contact.address);
+	    driver.findElement(By.name("home")).clear();
+	    driver.findElement(By.name("home")).sendKeys(contact.homephone);
+	    driver.findElement(By.name("mobile")).clear();
+	    driver.findElement(By.name("mobile")).sendKeys(contact.mobilephone);
+	    driver.findElement(By.name("work")).clear();
+	    driver.findElement(By.name("work")).sendKeys(contact.workphone);
+	    driver.findElement(By.name("email")).clear();
+	    driver.findElement(By.name("email")).sendKeys(contact.firstemail);
+	    driver.findElement(By.name("email2")).clear();
+	    driver.findElement(By.name("email2")).sendKeys(contact.secondemail);
+	    new Select(driver.findElement(By.name("bday"))).selectByVisibleText(contact.day);
+	    new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText(contact.month);
+	    driver.findElement(By.name("byear")).clear();
+	    driver.findElement(By.name("byear")).sendKeys(contact.year);
+	    new Select(driver.findElement(By.name("new_group"))).selectByVisibleText(contact.groupname);
+	    driver.findElement(By.name("address2")).clear();
+	    driver.findElement(By.name("address2")).sendKeys(contact.secondaddress);
+	    driver.findElement(By.name("phone2")).clear();
+	    driver.findElement(By.name("phone2")).sendKeys(contact.secondphone);
 	}
 
 	public void initContactCreation() {
-		TestBase.driver.findElement(By.linkText("add new")).click();
+		driver.findElement(By.linkText("add new")).click();
 	}
 
 	// some additional methods for elements location and alert handling	
 	
 	public boolean isElementPresent(By by) {
 	    try {
-	      TestBase.driver.findElement(by);
+	      driver.findElement(by);
 	      return true;
 	    } catch (NoSuchElementException e) {
 	      return false;
@@ -98,7 +126,7 @@ public class ApplicationManager {
 
 	public boolean isAlertPresent() {
 	    try {
-	      TestBase.driver.switchTo().alert();
+	      driver.switchTo().alert();
 	      return true;
 	    } catch (NoAlertPresentException e) {
 	      return false;
@@ -107,17 +135,18 @@ public class ApplicationManager {
 
 	public String closeAlertAndGetItsText() {
 	    try {
-	      Alert alert = TestBase.driver.switchTo().alert();
+	      Alert alert = driver.switchTo().alert();
 	      String alertText = alert.getText();
-	      if (TestBase.acceptNextAlert) {
+	      if (acceptNextAlert) {
 	        alert.accept();
 	      } else {
 	        alert.dismiss();
 	      }
 	      return alertText;
 	    } finally {
-	      TestBase.acceptNextAlert = true;
+	      acceptNextAlert = true;
 	    }
 	  }
+
 
 }
--- adressbook-selenium-tests/src/com/example/tests/TestBase.java
+++ adressbook-selenium-tests/src/com/example/tests/TestBase.java
@@ -1,11 +1,5 @@
 package com.example.tests;
 
-import static org.junit.Assert.fail;
-
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
@@ -15,25 +9,15 @@ public class TestBase {
 	
 	protected ApplicationManager app;
 
-	public static  WebDriver driver;
-	public static String baseUrl;
-	public static boolean acceptNextAlert = true;
-	private static StringBuffer verificationErrors = new StringBuffer();
 
 	@BeforeTest
 	public void setUp() throws Exception {
-	    driver = new FirefoxDriver();
-	    baseUrl = "http://localhost/";
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	  }
+		app = new ApplicationManager();
+	}
 
 	@AfterTest
 	public void tearDown() throws Exception {
-	    driver.quit();
-	    String verificationErrorString = verificationErrors.toString();
-	    if (!"".equals(verificationErrorString)) {
-	      fail(verificationErrorString);
-	    }
-	  }
+		app.stop();
+	}
 	
 }
