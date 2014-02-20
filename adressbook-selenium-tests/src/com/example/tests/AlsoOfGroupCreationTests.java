7370df6c689294b3a510051f755a49d090a8c3ec
runigma <nigmatullin.ruslan@gmail.com>
2014-02-20 15:53:39 +0400

    New check was added to AlsOfGroupCreationTests
    New check was added to AlsOfGroupCreationTests. Now List of groups
    before and after group creation are compared.

--- adressbook-selenium-tests/src/com/example/tests/AlsoOfGroupCreationTests.java
+++ adressbook-selenium-tests/src/com/example/tests/AlsoOfGroupCreationTests.java
@@ -1,5 +1,10 @@
 package com.example.tests;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 public class AlsoOfGroupCreationTests extends TestBase {  
@@ -7,9 +12,23 @@ public class AlsoOfGroupCreationTests extends TestBase {
   public void testEmptyGroupCreation() throws Exception {
     app.getNavigationHelper().openMainPage();
     app.getGroupHelper().gotoGroupsPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("", "", ""));
+    
+    //save old state
+    List<GroupData> oldList = app.getGroupHelper().getGroups();
+    
+    //actions
+    GroupData group = new GroupData("", "", "");
+    app.getGroupHelper().initGroupCreation();    
+	app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupsPage();
+    
+    //save new state
+    List<GroupData> newList = app.getGroupHelper().getGroups();
+    
+    //compare states
+    oldList.add(group);
+    Collections.sort(oldList);
+    assertEquals(newList, oldList);    
   }
 }
