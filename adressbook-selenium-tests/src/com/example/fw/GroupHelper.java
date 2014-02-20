ca25c0acc225c76f0cfcd7278088d4edcff7749b
runigma <nigmatullin.ruslan@gmail.com>
2014-02-20 16:11:13 +0400

    New check was added to GroupRemoval and GroupModification tests
    New check was added to GroupRemoval and GroupModification tests. Group
    lists before and after removing/modification are compared.

--- adressbook-selenium-tests/src/com/example/fw/GroupHelper.java
+++ adressbook-selenium-tests/src/com/example/fw/GroupHelper.java
@@ -42,7 +42,7 @@ public class GroupHelper extends HelperBase {
 	}
 
 	private void selectGroupByIndex(int index) {
-		click(By.xpath("//input[@name='selected[]'][" + index + "]"));
+		click(By.xpath("//input[@name='selected[]'][" + (index+1) + "]"));
 	}
 
 	public void initGroupModification(int index) {
