70008c8cf54e10a611db81aa550122b8e5b23737
runigma <nigmatullin.ruslan@gmail.com>
2014-02-20 12:53:04 +0400

    New assertion was added to GroupCreationTests
    New assertion was added to GroupCreationTest class. This assertion
    compare size of list of group before and after group creation. New
    method getGroups also added to GroupHelper class.

--- adressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ adressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,5 +1,8 @@
 package com.example.tests;
 
+import java.util.List;
+import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
@@ -7,6 +10,11 @@ public class GroupCreationTests extends TestBase {
   public void testNonEmptyGroupCreation() throws Exception {
     app.getNavigationHelper().openMainPage();
     app.getGroupHelper().gotoGroupsPage();
+    
+    //save old state
+    List<GroupData> oldList = app.getGroupHelper().getGroups();
+    
+    //actions
     app.getGroupHelper().initGroupCreation();
     GroupData group = new GroupData();
 	group.name = "group name 1";
@@ -15,5 +23,11 @@ public class GroupCreationTests extends TestBase {
     app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupsPage();
+    
+    //save new state
+    List<GroupData> newList = app.getGroupHelper().getGroups();
+    
+    //compare states
+    assertEquals(newList.size(),oldList.size()+1);
   }
 }
