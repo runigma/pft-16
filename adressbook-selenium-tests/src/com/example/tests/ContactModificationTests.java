5da5ffa0dc3fc5e3cec9985dc92268b6540fcbeb
runigma <nigmatullin.ruslan@gmail.com>
2014-02-20 18:03:21 +0400

    New check was added for ContactModificationTests
    New check was added for ContactModificationTests. List of contacts
    before and after modification are compared.

--- adressbook-selenium-tests/src/com/example/tests/ContactModificationTests.java
+++ adressbook-selenium-tests/src/com/example/tests/ContactModificationTests.java
@@ -1,18 +1,39 @@
 package com.example.tests;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 public class ContactModificationTests extends TestBase {
 	
 	@Test
 	public void modifySomeContact(){
-		app.getNavigationHelper().openMainPage();		
-		app.getContactHelper().initContactModification(1);
+		app.getNavigationHelper().openMainPage();
+		
+		//save old state
+	    List<ContactData> oldList = app.getContactHelper().getContacts();
+	    
+	    //actions
+		app.getContactHelper().initContactModification(0);
 		ContactData contact = new ContactData();
 		contact.firstname = "NEW FIRST NAME 1";
+		contact.lastname = "NEW LAST NAME 1";
 		app.getContactHelper().fillContactForm(contact);
 		app.getContactHelper().submitContactModification();
 		app.getNavigationHelper().returnToHomePage();
+		
+		//save new state
+	    List<ContactData> newList = app.getContactHelper().getContacts();
+	    
+	    //compare states
+	    contact.firstname = contact.firstname + " " + contact.lastname;
+	    oldList.remove(0);
+	    oldList.add(contact);
+	    Collections.sort(oldList);
+	    assertEquals(newList, oldList);
 	}
 
 }
