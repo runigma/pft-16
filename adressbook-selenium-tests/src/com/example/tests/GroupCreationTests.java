967a53b9b1cdd3cd0f97a3772d5f33f073ade4aa
runigma <nigmatullin.ruslan@gmail.com>
2014-02-24 00:25:36 +0400

    Test testNonEmptyGroupCreation modified
    testNonEmptyGroupCreation renamed to testGroupCreationWithValidData.
    randomValidGroupGenerator DataProvider was created.
    randomValidGroupGenerator fill group fields by random values.

--- adressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ adressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,15 +1,43 @@
 package com.example.tests;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 import static org.testng.Assert.assertEquals;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
-  @Test
-  public void testNonEmptyGroupCreation() throws Exception {
+	
+	@DataProvider
+	public Iterator<Object[]> randomValidGroupGenerator(){
+		List<Object[]> list = new ArrayList<Object[]>();		
+		for (int i=0; i<5; i++)
+		{
+			GroupData group = new GroupData();			
+			group.name = generateRandomString();
+			group.header = generateRandomString();
+			group.footer = generateRandomString();
+			list.add(new Object[]{group});
+		}
+		return list.iterator(); 
+	}
+	
+	public String generateRandomString(){
+		Random rnd = new Random();
+		if (rnd.nextInt(3) == 0) {
+			return "";
+		} else {
+			return "test" + rnd.nextInt();
+		}		
+	}
+	
+  @Test(dataProvider = "randomValidGroupGenerator")
+  public void testGroupCreationWithValidData(GroupData group) throws Exception {
     app.getNavigationHelper().openMainPage();
     app.getGroupHelper().gotoGroupsPage();
     
@@ -18,10 +46,6 @@ public class GroupCreationTests extends TestBase {
     
     //actions
     app.getGroupHelper().initGroupCreation();
-    GroupData group = new GroupData();
-	group.name = "group name 1";
-	group.header = "header 1";
-	group.footer = "footer 1";	
     app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupsPage();
