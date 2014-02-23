21bfc0a9f55f7bfd5aab08d7ef0b32cead2a5abc
runigma <nigmatullin.ruslan@gmail.com>
2014-02-24 00:42:44 +0400

    GroupModificationTests modified by using random index
    GroupModificationTests modified by using random index

--- adressbook-selenium-tests/src/com/example/tests/GroupModificationTests.java
+++ adressbook-selenium-tests/src/com/example/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import static org.testng.Assert.assertEquals;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import org.testng.annotations.Test;
 
@@ -17,8 +18,11 @@ public class GroupModificationTests extends TestBase{
 	    //save old state
 	    List<GroupData> oldList = app.getGroupHelper().getGroups();
 	    
+	    Random rnd = new Random();
+	    int index = rnd.nextInt(oldList.size()-1);
+	    
 	    //actions
-		app.getGroupHelper().initGroupModification(0);
+		app.getGroupHelper().initGroupModification(index);
 		GroupData group = new GroupData();
 		group.name = "new name";
 		app.getGroupHelper().fillGroupForm(group);
@@ -29,7 +33,7 @@ public class GroupModificationTests extends TestBase{
 	    List<GroupData> newList = app.getGroupHelper().getGroups();
 	    
 	    //compare states
-	    oldList.remove(0);
+	    oldList.remove(index);
 	    oldList.add(group);
 	    Collections.sort(oldList);
 	    assertEquals(newList, oldList);
